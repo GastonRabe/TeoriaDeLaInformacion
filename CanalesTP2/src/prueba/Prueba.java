c0c8d8d8fdddd06bccd7aebfe903e4b68e6edbc2
Valentín <89087929+ValentinGarciaIriart@users.noreply.github.com>
2021-10-24 10:52:40 -0300

    TA FLAMA

---
+++ CanalesTP2/src/prueba/Prueba.java
@@ -0,0 +1,19 @@
+package prueba;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+
+		Canal c = new Canal();
+
+		c.cargaDatos();
+		c.getMatCondicional();
+		c.getMatSucSimult();
+		c.getEntropiaAfin();
+		c.getResultados();		
+		c.getResultados2();
+
+
+	}
+
+}
