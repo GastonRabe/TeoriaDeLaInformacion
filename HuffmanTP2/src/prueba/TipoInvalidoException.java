d4de9f5aa69a06ad2a7f1db311f00df24c00047c
Valentín <89087929+ValentinGarciaIriart@users.noreply.github.com>
2021-11-02 14:12:47 -0300

    Add files via upload

---
+++ HuffmanTP2/src/prueba/TipoInvalidoException.java
@@ -0,0 +1,8 @@
+package prueba;
+
+public class TipoInvalidoException extends Exception {
+	public TipoInvalidoException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
