d8197e627c6d2795aa31d1244ef171b134527518
Valentín <89087929+ValentinGarciaIriart@users.noreply.github.com>
2021-11-04 15:21:51 -0300

    Delete HuffmanTP2 directory

--- HuffmanTP2/src/prueba/TipoInvalidoException.java
+++
@@ -1,8 +0,0 @@
-package prueba;
-
-public class TipoInvalidoException extends Exception {
-	public TipoInvalidoException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
-}
